c37221445f9b27d0d9b0cef7d50bd12ea7a44afa
DanFischer01 <miojo1412@gmail.com>
2026-04-07 07:28:37 -0300

    alterarAluno.php

---
+++ CRUD-DAW/alterarAluno.php
@@ -0,0 +1,29 @@
+<?php
+// Conexão simples
+$pdo = new PDO("mysql:host=localhost;dbname=escola", "root", "");
+
+// Pegar o ID do aluno que veio pela URL
+$id = $_GET['id'];
+
+// Buscar os dados atuais
+$sql = $pdo->prepare("SELECT * FROM alunos WHERE id = :id");
+$sql->bindValue(':id', $id);
+$sql->execute();
+$aluno = $sql->fetch(PDO::FETCH_ASSOC);
+
+if (!$aluno) {
+    die("Aluno não encontrado!");
+}
+?>
+
+<form action="atualizar.php" method="POST">
+    <input type="hidden" name="id" value="<?= $aluno['id']; ?>">
+
+    <label>Nome:</label>
+    <input type="text" name="nome" value="<?= $aluno['nome']; ?>"><br>
+
+    <label>E-mail:</label>
+    <input type="email" name="email" value="<?= $aluno['email']; ?>"><br>
+
+    <button type="submit">Salvar Alterações</button>
+</form>
